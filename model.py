378adb1bf049ec1627e1933c5c09f114fc90f84b
werdnaH <huzepengyx@gmail.com>
2021-12-12 00:14:06 -0500

    updated model of reset

--- model.py
+++ model.py
@@ -53,50 +53,12 @@ class SpinalNet_ResNet(nn.Module):
     
     def __init__(self):
         super(SpinalNet_ResNet, self).__init__()
-        
-        model_ft = models.wide_resnet101_2(pretrained=True)
-        num_ftrs = model_ft.fc.in_features
-        self.half_in_size = round(num_ftrs / 2)
-        layer_width = 540
-        Num_class = 100
-        
-        self.fc_spinal_layer1 = nn.Sequential(
-            #nn.Dropout(p = 0.5),
-            nn.Linear(self.half_in_size, layer_width),
-            #nn.BatchNorm1d(layer_width),
-            nn.ReLU(inplace=True),)
-        self.fc_spinal_layer2 = nn.Sequential(
-            #nn.Dropout(p = 0.5),
-            nn.Linear(self.half_in_size+layer_width, layer_width),
-            #nn.BatchNorm1d(layer_width),
-            nn.ReLU(inplace=True),)
-        self.fc_spinal_layer3 = nn.Sequential(
-            #nn.Dropout(p = 0.5),
-            nn.Linear(self.half_in_size+layer_width, layer_width),
-            #nn.BatchNorm1d(layer_width),
-            nn.ReLU(inplace=True),)
-        self.fc_spinal_layer4 = nn.Sequential(
-            #nn.Dropout(p = 0.5),
-            nn.Linear(self.half_in_size+layer_width, layer_width),
-            #nn.BatchNorm1d(layer_width),
-            nn.ReLU(inplace=True),)
-        self.fc_out = nn.Sequential(
-            #nn.Dropout(p = 0.5),
-            nn.Linear(layer_width*4, Num_class),)
+        self.model = models.resnet18(pretrained=True)
+        self.linear = nn.Linear(self.model.fc.in_features, 100)
 
     def forward(self, x):
-        x1 = self.fc_spinal_layer1(x[:, 0:self.half_in_size])
-        x2 = self.fc_spinal_layer2(torch.cat([ x[:,self.half_in_size:2*self.half_in_size], x1], dim=1))
-        x3 = self.fc_spinal_layer3(torch.cat([ x[:,0:self.half_in_size], x2], dim=1))
-        x4 = self.fc_spinal_layer4(torch.cat([ x[:,self.half_in_size:2*self.half_in_size], x3], dim=1))
-
-
-        x = torch.cat([x1, x2], dim=1)
-        x = torch.cat([x, x3], dim=1)
-        x = torch.cat([x, x4], dim=1)
-
-
-        x = self.fc_out(x)
+        x = self.model(x)
+        x = self.linear(x)
         return x
 
 class Vgg16(nn.Module):
